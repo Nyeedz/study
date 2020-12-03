6bc7e480f7462c0544a3b30faeb444c50cd1d801
Nyeedz <chr.alexfb@gmail.com>
2020-12-03 12:36:37 -0300

    update feedback collection node fullstack study

---
+++ fullstack/feedback-collection/server/config/keys.js
@@ -0,0 +1,10 @@
+module.exports = {
+  googleClientID:
+    '727231623839-boh5tm9asd9s4ajl4jedn4l7isq2hclh.apps.googleusercontent.com',
+  googleClientSecret: '3DhI-ZS-UmgYSntnlQe1oXRD',
+  facebookClientID: '345295963336501',
+  facebookClientSecret: '8a618b550c78199c716237522068f75d',
+  mongoURI:
+    'mongodb+srv://nyeedz:17091995@emaily.1qbml.mongodb.net/emaily?retryWrites=true&w=majority',
+  cookieKey: 'qwdqwdqhsasqdgqhwjdgqwikdhsqdsadquwdqwdhsakjlç',
+};
