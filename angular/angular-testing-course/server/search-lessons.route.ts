fbc37bd5fb39165d5b2e24d24aa2806a254fb90c
Nyeedz <chr.alexfb@gmail.com>
2020-12-07 12:21:49 -0300

    fix search and pagination

--- angular/angular-testing-course/server/search-lessons.route.ts
+++ angular/angular-testing-course/server/search-lessons.route.ts
@@ -3,11 +3,35 @@ import { LESSONS } from './db-data';
 import { setTimeout } from 'timers';
 
 export function searchLessons(req: Request, res: Response) {
-  const courseId: number = +req.query.courseId;
+  const courseId: number = +req.query.courseId,
+    filter = req.query.filter || '',
+    sortOrder = req.query.sortOrder,
+    pageNumber: number = +req.query.pageNumber || 0,
+    pageSize: number = +req.query.pageSize;
 
-  res.status(200).json({
-    payload: Object.values(LESSONS).filter(
-      lesson => lesson.courseId === courseId
-    )
-  });
+  let lessons = Object.values(LESSONS)
+    .filter(lesson => lesson.courseId == courseId)
+    .sort((l1, l2) => l1.id - l2.id);
+
+  if (filter) {
+    lessons = lessons.filter(
+      lesson =>
+        lesson.description
+          .trim()
+          .toLowerCase()
+          .search(filter.toString().toLowerCase()) >= 0
+    );
+  }
+
+  if (sortOrder == 'desc') {
+    lessons = lessons.reverse();
+  }
+
+  const initialPos = pageNumber * pageSize;
+
+  const lessonsPage = lessons.slice(initialPos, initialPos + pageSize);
+
+  setTimeout(() => {
+    res.status(200).json({ payload: lessonsPage });
+  }, 1000);
 }
