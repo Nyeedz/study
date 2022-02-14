1619b96f7444cfb0678cb1644580069ac710e196
Christian Álex Fernandes Barbosa <chr.alexfb@gmail.com>
2022-02-14 01:02:04 -0300

    initial commit

---
+++ react/data-fetching/src/App.tsx
@@ -0,0 +1,27 @@
+import './App.css';
+import { useFetch } from './hooks/useFetch';
+
+type Repository = {
+  full_name: string;
+  description: string;
+};
+
+function App() {
+  const { data: repositories, isFetching } = useFetch<Repository[]>('/users/nyeedz/repos');
+
+  return (
+    <ul>
+      {isFetching && <p>Carregando...</p>}
+      {repositories?.map((repo: Repository) => {
+        return (
+          <li key={repo.full_name}>
+            <strong>{repo.full_name}</strong>
+            <p>{repo.description}</p>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+export default App;
